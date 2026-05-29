760735bfa01cd526871dc99149f68d6cb717025f
Rémi Renard <remi.renard@telecom-paris.fr>
2026-05-29 23:57:34 +0200

    Suppression des caractères inutiles des mots

--- scrapping.py
+++ scrapping.py
@@ -1,6 +1,5 @@
 #%%
 import pdfplumber
-cours1 = r".\cours1.pdf"
 
 pages_par_feuille = 2
 dico_mots = dict()
@@ -8,7 +7,14 @@ dico_mots = dict()
 def estValide(mot):
     return True
 
-def add_words(file,pages_par_feuille, offset):
+def formatage(mot):
+    symboles = ['(',')','.',',','!','?',';','"']
+    m = mot
+    for s in symboles:
+        m=m.replace(s,'')
+    return m
+
+def ajout_mots(file,pages_par_feuille, offset):
 
     with pdfplumber.open(file) as pdf:
         pages = pdf.pages
@@ -16,24 +22,26 @@ def add_words(file,pages_par_feuille, offset):
         for i in range(len(pages)):
             mots = pages[i].extract_text().split()
             for mot in mots:
-                valide = estValide(mot)
+                mot2 = formatage(mot)
+                valide = estValide(mot2)
                 num_page = (int) (i/pages_par_feuille + offset)
-                if( valide and not(mot in dico_mots)):
-                    dico_mots[mot] = [num_page]
-                elif (valide and not(num_page in dico_mots[mot])):
-                    dico_mots[mot].append(num_page)
+                if( valide and not(mot2 in dico_mots)):
+                    dico_mots[mot2] = [num_page]
+                elif (valide and not(num_page in dico_mots[mot2])):
+                    dico_mots[mot2].append(num_page)
 
 def main():
 
-    add_words(r".\cours1.pdf",pages_par_feuille,5)
-    add_words(r".\cours2.pdf",pages_par_feuille,33)
-    add_words(r".\cours3.pdf",pages_par_feuille,63)
-    add_words(r".\cours4.pdf",pages_par_feuille,83)
-    add_words(r".\cours5.pdf",pages_par_feuille,101)
-    add_words(r".\cours6.pdf",pages_par_feuille,155)
+    ajout_mots(r".\cours1.pdf",pages_par_feuille,5)
+    ajout_mots(r".\cours2.pdf",pages_par_feuille,33)
+    ajout_mots(r".\cours3.pdf",pages_par_feuille,63)
+    ajout_mots(r".\cours4.pdf",pages_par_feuille,83)
+    ajout_mots(r".\cours5.pdf",pages_par_feuille,101)
+    ajout_mots(r".\cours6.pdf",pages_par_feuille,155)
             
 
 main()
 #%%
-print(dico_mots["(upwelling)"])
+print(dico_mots["upwelling"])
+print(len(dico_mots))
 # %%
